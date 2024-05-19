3cada3f30a4d849636a9f6096eb98ba3772e18c1
logkoege <togokoegele@gmail.com>
2024-05-19 03:23:24 +0200

    l

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 02:35:41 by logkoege          #+#    #+#             */
+/*   Updated: 2024/05/19 02:54:46 by logkoege         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char 	*get_next_line(int fd);
+
+#endif
